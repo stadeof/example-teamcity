56229e2029002ba3c5d886a711af2e9dc84025fd
stadeoff <stadeoff@mail.ru>
2023-08-01 12:51:07 +0300

    add test for netology

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -10,6 +10,9 @@ public class Welcomer{
 	public String sayNeedGold(){
 		return "Not enough gold";
 	}
+	public String sayReplHunter(){
+		return "Ya ne znayu java hunter";
+	}
 	public String saySome(){
 		return "something in the way";
 	}
